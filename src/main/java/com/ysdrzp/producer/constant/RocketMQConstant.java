2ccfacc542fcd1384c8d2689a6a62eda0f5a7a8e
zhaizhipeng <zhaizhipeng@extracme.com>
2020-04-21 13:59:53 +0800

    RocketMQ 快速入门

---
+++ src/main/java/com/ysdrzp/producer/constant/RocketMQConstant.java
@@ -0,0 +1,13 @@
+package com.ysdrzp.producer.constant;
+
+/**
+ * RocketMQ 常量配置
+ */
+public class RocketMQConstant {
+
+    /**
+     * NAME_SERVER 地址
+     */
+    public static final String NAME_SERVER = "106.14.190.145:9876";
+
+}
---
+++ src/main/java/com/ysdrzp/producer/rocketmq/quick/Producer.java
@@ -0,0 +1,33 @@
+package com.ysdrzp.producer.rocketmq.quick;
+
+import com.ysdrzp.producer.constant.RocketMQConstant;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+public class Producer {
+
+    public static void main(String[] args) throws Exception {
+        //Instantiate with a producer group name.
+        DefaultMQProducer producer = new DefaultMQProducer("quick_producer_group");
+        // Specify name server addresses.
+        producer.setNamesrvAddr(RocketMQConstant.NAME_SERVER);
+
+        //Launch the instance.
+        producer.start();
+        for (int i = 0; i < 15; i++) {
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("quick_topic",
+                    "quick_tag",
+                    ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET)
+            );
+            //Call send message to deliver message to one of brokers.
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+        //Shut down once the producer instance is not longer in use.
+        producer.shutdown();
+    }
+
+}
